b443a9b08e1c810d2a9a671eb410a476f04a0f74
Adam McFadden & David Garber <student@epicodus.com>
2015-05-06 12:02:07 -0700

    returns an empty array.

--- lib/authors.rb
+++ lib/authors.rb
@@ -5,4 +5,16 @@ class Author
     @author = attributes.fetch(:author)
     @id = attributes.fetch(:id)
   end
+
+  define_singleton_method(:all) do
+    all_authors = DB.exec('SELECT * FROM authors')
+    authors = []
+    all_authors.each() do |author|
+      author = author.fetch(:author)
+      id = author.fetch(:id).to_i()
+      authors.push(Author.new({:author => author, :id => id}))
+    end
+    authors
+
+  end
 end
--- spec/authors_spec.rb
+++ spec/authors_spec.rb
@@ -8,5 +8,11 @@ describe(Author) do
       expect(author_test.author()).to(eq('Terry Pratchet'))
     end
   end
-  
+
+  describe(".all") do
+    it("returns an empty array") do
+      expect(Author.all()).to(eq([]))
+    end
+  end
+
 end
